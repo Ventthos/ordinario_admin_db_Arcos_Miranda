e540f21a54b44f9df1014f8dc3680ee20307b669
Ventthos <ferra04@live.com.mx>
2024-12-05 09:18:39 -0600

    First Push: Add docker support

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const res = require('express/lib/response');
+const {json} = require('express/lib/response');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Hello World</h1>');
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
