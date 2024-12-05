5be7b135ba09fcb01ff25ff9c79694dee9e80b37
Ventthos <ferra04@live.com.mx>
2024-12-05 12:49:03 -0600

    Método post

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.get('/api/estudiantes', async (req, res)=>{
     res.json(estudiantes);
 });
 
+app.post('/api/estudiantes', async (req, res)=>{
+    const {nombre, apellidos, email, matricula, edad, semestre} = req.body;
+    const result = await db.query(`INSERT INTO estudiantes (nombre, apellidos, email, matricula,
+        edad, semestre, usuario_creacion, fecha_creacion) VALUES (?, ?, ?, ?,?,?,?,NOW())`, [nombre, apellidos, email, matricula, edad, semestre, "admin"]);
+    res.json(result);
+});
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 });
